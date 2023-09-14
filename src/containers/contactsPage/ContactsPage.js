0b9d7b8045b8dc326786c225e185ac73f1db2eb7
Gabriele.Sinis <gabrielesinis.gs@gmail.com>
2023-09-14 09:09:04 +0100

    Task 3 Completed

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,33 +3,59 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ({ contacts, addContact }) => {
+  //Define state variables for contact info and duplicate check
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  //Check for duplicates whenever the name in the form changes and indicate the name is a duplicate
+  const [isDuplicate, setIsDuplicate] = useState(false); 
+
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    e.preventDefault();  
+
+  // Check for duplicate contact names
+   const isNameDuplicate = contacts.some((contacts) => contacts.name === name);
+
+   if (!isNameDuplicate){
+    // Add contact and clear form data
+    addContact({ name, phone, email });
+    setName('');
+    setPhone('');
+    setEmail('');
+    setIsDuplicate(false);
+  } else {
+    // Alert user to duplicate contact name
+    setIsDuplicate(true);
+   }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  useEffect(() => {
+    // Reset multiple check when the name changes
+    setIsDuplicate(false);
+  }, [name]);
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+        name={name}
+        setName={setName}
+        phone={phone}
+        setPhone={setPhone}
+        email={email}
+        setEmail={setEmail}
+        handleSubmit={handleSubmit}
+        isDuplicate={isDuplicate}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
